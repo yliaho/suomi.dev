7457fcc53fad871747ad6e7038cd02aba64ff241
Joonas Yliaho <joonasyliaho@Joonass-MacBook-Pro.local>
2020-08-19 20:39:57 +0300

    why

--- lib/suomidev/release.ex
+++ lib/suomidev/release.ex
@@ -1,25 +1,18 @@
 defmodule Suomidev.Release do
   @app :suomidev
 
-  def migrate() do
-    load_app()
-
+  def migrate do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
-    load_app()
-
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  defp repos() do
-    Application.fetch_env!(@app, :ecto_repos)
-  end
-
-  defp load_app() do
+  defp repos do
     Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
   end
 end
