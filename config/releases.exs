5e364889c98c68659f383016399bb55aa2f75b15
Joonas Yliaho <joonasyliaho@Joonass-MacBook-Pro.local>
2020-08-19 20:23:27 +0300

    why

--- config/releases.exs
+++ config/releases.exs
@@ -4,10 +4,3 @@ config :suomidev, SuomidevWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   url: [host: nil, port: 443]
-
-config :suomidev, Suomidev.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
-  ssl: true,
-  pool_size: 9
