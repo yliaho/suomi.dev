0d243ee41c01b027af0a82bbb7353702d376cf41
Joonas Yliaho <joonasyliaho@Joonass-MacBook-Pro.local>
2020-08-20 10:37:10 +0300

    purgecss whitelisting fix

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = (env, options) => {
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
         new OptimizeCSSAssetsPlugin({}),
         new PurgecssPlugin({
-          whitelist: ["input", ".upvote-btn", ".upvote-btn[data-liked]", "&::placeholder"],
+          whitelist: ["input", "upvote-btn", "upvote-btn[data-liked]", "&::placeholder"],
           paths: globAll.sync([
             '../lib/suomidev_web/templates/**/*.html.eex',
             '../lib/suomidev_web/views/**/*.ex',
