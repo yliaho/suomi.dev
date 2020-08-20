f3cd78913650368aea90d71ffe6f8145dd43f9bc
Joonas Yliaho <joonasyliaho@Joonass-MacBook-Pro.local>
2020-08-20 10:56:07 +0300

    remove `ensure_all_started` in release migrate/0

--- lib/suomidev/release.ex
+++ lib/suomidev/release.ex
@@ -1,7 +1,5 @@
 defmodule Suomidev.Release do
   def migrate do
-    {:ok, _} = Application.ensure_all_started(:suomidev)
-
     path = Application.app_dir(:suomidev, "priv/repo/migrations")
 
     Ecto.Migrator.run(Suomidev.Repo, path, :up, all: true)
