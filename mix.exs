f1d6588c85d7a451f91ced65837fc7ac84707feb
yliaho <joonas.yliaho@gmail.com>
2020-08-22 14:47:56 +0300

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Suomidev.MixProject do
   def project do
     [
       app: :suomidev,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
